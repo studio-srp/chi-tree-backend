4966eb20819bd5f2d37d3aa3eea2d28b9249954f
Mshihan <167sihan@gmail.com>
2022-04-28 11:55:00 +0530

    created access control allow

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 
 app.use("/patientdetails", PatientDetailsRoute);
