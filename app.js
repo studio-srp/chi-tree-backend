560388b34c2d23c47282660472c5702525a1ddc7
Mshihan <167sihan@gmail.com>
2022-05-11 10:57:59 +0530

    added front end

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const PatientDetailsRoute = require("./routes/PatientDetailsRoute");
 const BioMarkersRoute = require("./routes/BioMarkersRoute");
 const AppError = require("./utils/appError");
-const errorController = require("./controllers/ErrorController");
+// const errorController = require("./controllers/ErrorController");
 const AuthenticationDetailsRoute = require("./routes/AuthenticationDetailsRoute");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
+app.use(express.static(path.join("public")));
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
@@ -15,31 +17,16 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/patientdetails", PatientDetailsRoute);
-app.use("/biomarkers", BioMarkersRoute);
-app.use("/auth", AuthenticationDetailsRoute);
+app.use("/api/patientdetails", PatientDetailsRoute);
+app.use("/api/biomarkers", BioMarkersRoute);
+app.use("/api/auth", AuthenticationDetailsRoute);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on the server`));
 });
 
-app.use(errorController);
+app.use((req, res, next) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 module.exports = app;
-
-// let value = 21;
-
-// let optimal = 29;
-
-// let diviation = Math.floor((value / optimal) * 50);
-
-// console.log(diviation);
-
-// let points;
-// if (diviation > 50) {
-//   points = 100 - (diviation - 50) * 2;
-// } else {
-//   points = diviation * 2;
-// }
-
-// console.log(points);
