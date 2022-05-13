5029e3bd6c490fa945215e0b083b19641ccacdbc
Mshihan <167sihan@gmail.com>
2022-05-13 09:15:59 +0530

    changed the model section

--- models/patientModel.js
+++ models/patientModel.js
@@ -18,7 +18,7 @@ const patientSchema = mongoose.Schema({
       labName: { type: String, required: [true, "Lab name is required"] },
     },
   ],
-  isAdmin: { type: Boolean, defualt: false },
+  isAdmin: { type: Boolean, default: false },
 });
 
 patientSchema.pre(/^find/, function (next) {
