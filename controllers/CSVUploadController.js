992de3d404332784fb83657d2245580d8b224328
Mshihan <167sihan@gmail.com>
2022-06-21 11:27:48 +0530

    changed

--- controllers/CSVUploadController.js
+++ controllers/CSVUploadController.js
@@ -9,113 +9,111 @@ const bcrypt = require("bcrypt");
 // const { updateUser } = require("./PatientDetailsController");
 
 exports.uploadCSV = catchAsync(async (req, res, next) => {
-  let uploadPath = path.join(__dirname, "..", "uploads", "uploaded.csv");
+    let uploadPath = path.join(__dirname, "..", "uploads", "uploaded.csv");
 
-  const file = req.files.csvFile;
+    const file = req.files.csvFile;
 
-  file.mv(uploadPath, (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-
-  const json = await csv().fromFile(uploadPath);
-
-  const biomarkerList = await Biomarker.find();
-
-  let newReports = [];
-
-  await Promise.all(
-    json.map(async (row) => {
-      const {
-        firstName,
-        lastName,
-        gender,
-        email,
-        username,
-        dob,
-        testDate,
-        labName,
-      } = row;
-
-      const existedBiomarkers = [];
-
-      biomarkerList.map((biomarker) => {
-        if (biomarker.name in row) {
-          if (row[`${biomarker.name}`]) {
-            if (biomarker.name) {
-              let deviation = Math.floor(
-                (row[`${biomarker.name}`] / biomarker.rangeOptimal) * 50
-              );
-              let points;
-              if (deviation > 100) {
-                points = 0;
-              } else if (deviation > 50) {
-                points = 100 - (deviation - 50) * 2;
-              } else {
-                points = deviation * 2;
-              }
-
-              existedBiomarkers.push({
-                biomarker: biomarker._id,
-                biomarkerValue: row[`${biomarker.name}`],
-                score: points,
-              });
-            }
-          } else {
-            return;
-          }
+    file.mv(uploadPath, (err) => {
+        if (err) {
+            console.log(err);
         }
-      });
-
-      // Create the report
-      const report = await Report.create({
-        biomarkers: existedBiomarkers,
-      });
-
-      let testedAt = new Date(testDate);
-      testedAt.setDate(testedAt.getDate() + 1);
-
-      newReports.push({
-        reportId: report._id,
-        testedAt,
-        labName,
-      });
-    })
-  );
-
-  // Checking the patient credentials
-  const patient = await Patient.findOne({ email: json[0].email });
-
-  // If the patient exists
-  if (patient) {
-    let { reports } = patient;
-
-    reports = [...reports, ...newReports];
-
-    await Patient.findByIdAndUpdate(
-      { _id: patient._id },
-      { reports: reports },
-      { new: true, runValidators: false }
-    );
-    //If there is no patient exists
-  } else {
-    let hashPassword = await bcrypt.hash("12345", 10);
-
-    const { firstName, lastName, gender, email, username, dob } = json[0];
-
-    // Create Patient Details
-    const patientCreated = await Patient.create({
-      firstName,
-      lastName,
-      dob,
-      gender,
-      email,
-      username,
-      isAdmin: false,
-      password: hashPassword,
-      reports: newReports,
     });
-  }
-  res.status(200).json({ status: "success" });
+
+    const json = await csv().fromFile(uploadPath);
+
+    const biomarkerList = await Biomarker.find();
+
+    let newReports = [];
+
+    await Promise.all(
+        json.map(async (row) => {
+            const { firstName, lastName, gender, email, username, dob, testDate, labName } = row;
+
+            const existedBiomarkers = [];
+
+            biomarkerList.map((biomarker) => {
+                if (biomarker.name in row) {
+                    if (row[`${biomarker.name}`]) {
+                        if (biomarker.name) {
+                            let deviation = Math.floor(
+                                (row[`${biomarker.name}`] / biomarker.rangeOptimal) * 50
+                            );
+                            let points;
+                            if (deviation > 100) {
+                                points = 0;
+                            } else if (deviation > 50) {
+                                points = 100 - (deviation - 50) * 2;
+                            } else {
+                                points = deviation * 2;
+                            }
+
+                            existedBiomarkers.push({
+                                biomarker: biomarker._id,
+                                biomarkerValue: row[`${biomarker.name}`],
+                                score: points,
+                            });
+                        }
+                    } else {
+                        return;
+                    }
+                }
+            });
+
+            // Create the report
+            const report = await Report.create({
+                biomarkers: existedBiomarkers,
+            });
+
+            newReports.push({
+                reportId: report._id,
+                testedAt: testDate,
+                labName,
+            });
+        })
+    );
+
+    // Checking the patient credentials
+    const patient = await Patient.findOne({ email: json[0].email });
+
+    // If the patient exists
+    if (patient) {
+        let { reports } = patient;
+
+        reports = [...reports, ...newReports];
+
+        await Patient.findByIdAndUpdate(
+            { _id: patient._id },
+            { reports: reports },
+            { new: true, runValidators: false }
+        );
+        //If there is no patient exists
+    } else {
+        let hashPassword = await bcrypt.hash("12345", 10);
+
+        const { firstName, lastName, gender, email, username, dob } = json[0];
+
+        // Create Patient Details
+        const patientCreated = await Patient.create({
+            firstName,
+            lastName,
+            dob,
+            gender,
+            email,
+            username,
+            isAdmin: false,
+            password: hashPassword,
+            reports: newReports,
+        });
+    }
+    res.status(200).json({ status: "success" });
 });
+
+// const previousDate = new Date(
+//     new Date(new Date(report.testedAt).setDate(new Date(report.testedAt).getDate() - 1)).setHours(
+//         0,
+//         0,
+//         0,
+//         0
+//     )
+// );
+// const requestedDate = new Date(new Date(reportDate).setHours(0, 0, 0, 0));
--- controllers/PatientDetailsController.js
+++ controllers/PatientDetailsController.js
@@ -5,263 +5,254 @@ const catchAsync = require("./../utils/catchAsync");
 const bcrypt = require("bcrypt");
 
 exports.getPatientDetails = catchAsync(async (req, res, next) => {
-  const patient = await Patient.find();
-  res.status(200).json({ status: "success", patient });
+    const patient = await Patient.find();
+    res.status(200).json({ status: "success", patient });
 });
 
 exports.createPatientDetails = catchAsync(async (req, res, next) => {
-  const {
-    biomarkers,
-    firstName,
-    lastName,
-    dob,
-    gender,
-    email,
-    username,
-    password,
-    testedAt,
-    labName,
-    isUpdateReport,
-    isAdmin,
-  } = req.body;
+    const {
+        biomarkers,
+        firstName,
+        lastName,
+        dob,
+        gender,
+        email,
+        username,
+        password,
+        testedAt,
+        labName,
+        isUpdateReport,
+        isAdmin,
+    } = req.body;
 
-  let hashPassword = await bcrypt.hash(password, 10);
+    let hashPassword = await bcrypt.hash(password, 10);
 
-  let patientCheck = await Patient.findOne({
-    username,
-    email,
-  });
-
-  // Getting the relevent biomarker Id's from database
-  const object = await Promise.all(
-    biomarkers.map(async (marker) => {
-      const markerId = await BioMarker.findOne({ name: marker.name });
-
-      let diviation = Math.floor((marker.value / markerId.rangeOptimal) * 50);
-
-      let points;
-      if (diviation > 100) {
-        points = 0;
-      } else if (diviation > 50) {
-        points = 100 - (diviation - 50) * 2;
-      } else {
-        points = diviation * 2;
-      }
-
-      return {
-        biomarker: markerId._id,
-        biomarkerValue: marker.value,
-        score: points,
-      };
-    })
-  );
-
-  //   Create the report
-  const report = await Report.create({
-    biomarkers: object,
-  });
-
-  if (patientCheck) {
-    let reports = patientCheck.reports;
-    reports.push({
-      reportId: report._id,
-      testedAt,
-      labName,
+    let patientCheck = await Patient.findOne({
+        username,
+        email,
     });
 
-    await Patient.findByIdAndUpdate(
-      { _id: patientCheck._id },
-      { reports: reports },
-      { new: true, runValidators: false }
+    // Getting the relevent biomarker Id's from database
+    const object = await Promise.all(
+        biomarkers.map(async (marker) => {
+            const markerId = await BioMarker.findOne({ name: marker.name });
+
+            let diviation = Math.floor((marker.value / markerId.rangeOptimal) * 50);
+
+            let points;
+            if (diviation > 100) {
+                points = 0;
+            } else if (diviation > 50) {
+                points = 100 - (diviation - 50) * 2;
+            } else {
+                points = diviation * 2;
+            }
+
+            return {
+                biomarker: markerId._id,
+                biomarkerValue: marker.value,
+                score: points,
+            };
+        })
     );
 
-    res.status(200).json({ status: "success", message: patientCheck });
-  } else {
-    // Create Patient Details
-    const patient = await Patient.create({
-      firstName,
-      lastName,
-      dob,
-      gender,
-      email,
-      username,
-      isAdmin,
-      password: hashPassword,
-      reports: [
-        {
-          reportId: report.id,
-          testedAt,
-          labName,
-        },
-      ],
+    //   Create the report
+    const report = await Report.create({
+        biomarkers: object,
     });
 
-    res.status(200).json({ status: "success", patient });
-    // }
-  }
+    if (patientCheck) {
+        let reports = patientCheck.reports;
+        reports.push({
+            reportId: report._id,
+            testedAt,
+            labName,
+        });
+
+        await Patient.findByIdAndUpdate(
+            { _id: patientCheck._id },
+            { reports: reports },
+            { new: true, runValidators: false }
+        );
+
+        res.status(200).json({ status: "success", message: patientCheck });
+    } else {
+        // Create Patient Details
+        const patient = await Patient.create({
+            firstName,
+            lastName,
+            dob,
+            gender,
+            email,
+            username,
+            isAdmin,
+            password: hashPassword,
+            reports: [
+                {
+                    reportId: report.id,
+                    testedAt,
+                    labName,
+                },
+            ],
+        });
+
+        res.status(200).json({ status: "success", patient });
+        // }
+    }
 });
 
 exports.checkEmail = catchAsync(async (req, res, next) => {
-  const email = req.body.email;
-  const patient = await Patient.findOne({ email });
+    const email = req.body.email;
+    const patient = await Patient.findOne({ email });
 
-  if (patient) {
-    res.status(200).json({ status: "success", patient });
-  } else {
-    res.status(200).json({ status: "error", message: "patient is not exist" });
-  }
+    if (patient) {
+        res.status(200).json({ status: "success", patient });
+    } else {
+        res.status(200).json({ status: "error", message: "patient is not exist" });
+    }
 });
 
 exports.getReport = catchAsync(async (req, res, next) => {
-  const { reportDate, email } = req.body;
+    const { reportDate, email } = req.body;
 
-  const patient = await Patient.findOne({ email }).populate({
-    path: "reports.reportId",
-  });
+    const patient = await Patient.findOne({ email }).populate({
+        path: "reports.reportId",
+    });
 
-  const report = patient.reports.filter((report) => {
-    const previousDate = new Date(report.testedAt);
-    const requestedDate = new Date(reportDate);
-    if (previousDate.getTime() === requestedDate.getTime()) {
-      return report;
-    }
-  });
+    const report = patient.reports.filter((report) => {
+        const previousDate = new Date(report.testedAt);
+        const requestedDate = new Date(reportDate);
+        if (previousDate.getTime() === requestedDate.getTime()) {
+            return report;
+        }
+    });
 
-  res.status(200).json({
-    status: "success",
-    report,
-  });
+    res.status(200).json({
+        status: "success",
+        report,
+    });
 });
 
 exports.updateUser = catchAsync(async (req, res, next) => {
-  // update user
-  const { isUpdateReport, biomarkers, testedAt, labName, username, email } =
-    req.body;
+    // update user
+    const { isUpdateReport, biomarkers, testedAt, labName, username, email } = req.body;
 
-  // isUpdateReport is available
+    // isUpdateReport is available
 
-  const object = await Promise.all(
-    biomarkers.map(async (marker) => {
-      const markerId = await BioMarker.findOne({ name: marker.name });
-      let diviation = Math.floor((marker.value / markerId.rangeOptimal) * 50);
-      let points;
-      if (diviation > 100) {
-        points = 0;
-      } else if (diviation > 50) {
-        points = 100 - (diviation - 50) * 2;
-      } else {
-        points = diviation * 2;
-      }
-      return {
-        biomarker: markerId._id,
-        biomarkerValue: marker.value,
-        score: points,
-      };
-    })
-  );
+    const object = await Promise.all(
+        biomarkers.map(async (marker) => {
+            const markerId = await BioMarker.findOne({ name: marker.name });
+            let diviation = Math.floor((marker.value / markerId.rangeOptimal) * 50);
+            let points;
+            if (diviation > 100) {
+                points = 0;
+            } else if (diviation > 50) {
+                points = 100 - (diviation - 50) * 2;
+            } else {
+                points = diviation * 2;
+            }
+            return {
+                biomarker: markerId._id,
+                biomarkerValue: marker.value,
+                score: points,
+            };
+        })
+    );
 
-  // update reports according to the biomarker
+    // update reports according to the biomarker
 
-  const report = await Report.findByIdAndUpdate(
-    { _id: isUpdateReport.biomarkerListId },
-    {
-      biomarkers: object,
-    }
-  );
+    const report = await Report.findByIdAndUpdate(
+        { _id: isUpdateReport.biomarkerListId },
+        {
+            biomarkers: object,
+        }
+    );
 
-  let patient = await Patient.findOne({
-    username,
-    email,
-  });
+    let patient = await Patient.findOne({
+        username,
+        email,
+    });
 
-  const index = patient.reports.findIndex((report) => {
-    return report.id === isUpdateReport.reportId;
-  });
+    const index = patient.reports.findIndex((report) => {
+        return report.id === isUpdateReport.reportId;
+    });
 
-  patient.reports[index] = {
-    ...patient.reports[index],
-    reportId: report._id,
-    testedAt,
-    labName,
-  };
+    patient.reports[index] = {
+        ...patient.reports[index],
+        reportId: report._id,
+        testedAt,
+        labName,
+    };
 
-  const updatedPatient = await Patient.findByIdAndUpdate(
-    { _id: patient.id },
-    patient,
-    {
-      runValidators: false,
-    }
-  );
+    const updatedPatient = await Patient.findByIdAndUpdate({ _id: patient.id }, patient, {
+        runValidators: false,
+    });
 
-  res.status(200).json({ status: "success", updatedPatient });
+    res.status(200).json({ status: "success", updatedPatient });
 
-  //   res.status(200).json({ status: "success" });
-  // }
+    //   res.status(200).json({ status: "success" });
+    // }
 });
 
 exports.deleteUser = catchAsync(async (req, res, next) => {
-  const { email } = req.body;
+    const { email } = req.body;
 
-  const patient = await Patient.findOne({ email });
+    const patient = await Patient.findOne({ email });
 
-  // Delete Reports from the patients
-  if (patient.reports.length > 0) {
-    await Promise.all(
-      patient.reports.map(async (report) => {
-        const deleteReport = await Report.findByIdAndDelete({
-          _id: report.reportId._id,
-        });
-      })
-    );
-  }
+    // Delete Reports from the patients
+    if (patient.reports.length > 0) {
+        await Promise.all(
+            patient.reports.map(async (report) => {
+                const deleteReport = await Report.findByIdAndDelete({
+                    _id: report.reportId._id,
+                });
+            })
+        );
+    }
 
-  // Delete Patinet
-  const deletePatient = await Patient.findByIdAndDelete({ _id: patient._id });
+    // Delete Patinet
+    const deletePatient = await Patient.findByIdAndDelete({ _id: patient._id });
 
-  res
-    .status(200)
-    .json({ status: "success", message: "Successfully deleted the patient" });
+    res.status(200).json({ status: "success", message: "Successfully deleted the patient" });
 });
 
 exports.deleteReport = catchAsync(async (req, res, next) => {
-  const { reportDate, email } = req.body;
+    const { reportDate, email } = req.body;
 
-  let patient = await Patient.findOne({ email }).populate({
-    path: "reports.reportId",
-  });
+    let patient = await Patient.findOne({ email }).populate({
+        path: "reports.reportId",
+    });
 
-  const report = patient.reports.filter((report) => {
-    const previousDate = new Date(report.testedAt);
-    const requestedDate = new Date(reportDate);
-    if (previousDate.getTime() === requestedDate.getTime()) {
-      return report;
-    }
-  });
+    const report = patient.reports.filter((report) => {
+        const previousDate = new Date(report.testedAt);
+        const requestedDate = new Date(reportDate);
+        if (previousDate.getTime() === requestedDate.getTime()) {
+            return report;
+        }
+    });
 
-  const deleteReport = await Report.findByIdAndDelete({
-    _id: report[0].reportId._id,
-  });
+    const deleteReport = await Report.findByIdAndDelete({
+        _id: report[0].reportId._id,
+    });
 
-  const { reports } = patient;
+    const { reports } = patient;
 
-  const updatedReports = reports.filter((report) => {
-    const previousDate = new Date(report.testedAt);
-    const requestedDate = new Date(reportDate);
-    if (previousDate.getTime() !== requestedDate.getTime()) {
-      return report;
-    }
-  });
+    const updatedReports = reports.filter((report) => {
+        const previousDate = new Date(report.testedAt);
+        const requestedDate = new Date(reportDate);
+        if (previousDate.getTime() !== requestedDate.getTime()) {
+            return report;
+        }
+    });
 
-  patient.reports = updatedReports;
+    patient.reports = updatedReports;
 
-  const updatedPatient = await Patient.findByIdAndUpdate(
-    { _id: patient._id },
-    patient,
-    { runValidators: false }
-  );
+    const updatedPatient = await Patient.findByIdAndUpdate({ _id: patient._id }, patient, {
+        runValidators: false,
+    });
 
-  res.status(200).json({
-    status: "success",
-    report,
-  });
+    res.status(200).json({
+        status: "success",
+        report,
+    });
 });
